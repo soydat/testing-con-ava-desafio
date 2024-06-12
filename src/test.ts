9da443ef42eb100c9fd8da150adf8845f105a948
soydat <classicwtink@gmail.com>
2024-06-12 05:52:13 -0300

    correctly tested the index classes

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,5 @@
 import test from "ava";
-import { Product } from "./index";
+import { Product, User } from "./index";
 
 test("Testeo la clase Product", (t) => {
   // test Product class
@@ -7,4 +7,12 @@ test("Testeo la clase Product", (t) => {
   t.is(product.name, "Product 1");
   t.is(product.price, 10);
 });
-test("Testeo la clase User y sus métodos", (t) => {});
+test("Testeo la clase User y sus métodos", (t) => {
+  const usuario = new User ("Nombre");
+  t.is(usuario.name, "Nombre");
+
+  const producto = new Product("leche", 1200);
+  usuario.addProduct(producto);
+  t.is(usuario.products.length, 1);
+  t.is(usuario.products[0], producto);
+});
